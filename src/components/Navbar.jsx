4c86d3b01d191fd2c2ab6de7ea22a5b18dd9eea5
brianmureithi <brianmurithi65@gmail.com>
2023-02-18 21:11:43 +0300

    Complete navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,12 @@ function Navbar() {
   return (
     <nav className='h-[10vh] flex flex-row justify-evenly border-b border-b-sky-300' >
         <div className='flex items-center'>
-            <h1 className='text-2xl font-bold tracking-widest text-orange-500'>ECIP</h1>
+            <h1 className='text-2xl font-bold tracking-widest text-orange-500'>E.C.I.P</h1>
         </div>
         <div className='flex flex-row justify-evenly w-1/4 items-end'>
-            <p className='bg-sky-500 text-slate-100 px-2 py-1 hover:bg-sky-600 cursor-pointer'>Home</p>
-            <p className='bg-sky-500 text-slate-100 px-2 py-1 hover:bg-sky-600 cursor-pointer'>About us</p>
-            <p className='bg-sky-500 text-slate-100 px-2 py-1 hover:bg-sky-600 cursor-pointer'>Contacts</p>
+            <p className='bg-sky-500 text-slate-100 px-2 py-1 hover:bg-sky-600 cursor-pointer rounded-t-md'>Home</p>
+            <p className='bg-sky-500 text-slate-100 px-2 py-1 hover:bg-sky-600 cursor-pointer rounded-t-md'>About us</p>
+            <p className='bg-sky-500 text-slate-100 px-2 py-1 hover:bg-sky-600 cursor-pointer rounded-t-md'>Contacts</p>
                 </div>
 
     </nav>
