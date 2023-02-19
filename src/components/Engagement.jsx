37f6c55b0ae105dee1a7f3a8311c729f92f6a5bd
brianmureithi <brianmurithi65@gmail.com>
2023-02-19 10:59:18 +0300

    Add footer

--- src/components/Engagement.jsx
+++ src/components/Engagement.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 function Engagement() {
   return (
   
-<div className=' w-full h-[88vh] bg-slate-100 absolute  mt-[182rem]  '>
+<div className=' w-full h-[92vh] bg-slate-100 absolute  mt-[182rem]  '>
   <h1 className='w-max mx-auto my-14 text-2xl font-semibold tracking-wider'>Engage with us</h1>
   <div className='flex flex-nowrap flex-row w-full justify-evenly items-center '>
   <div className='w-1/3 border h-[50vh] bg-white'>
     <img src='./images/img-7.jpg' className=' object-cover object-start max-h-[50vh] w-full rounded-md' alt='Engagement opportunity'/>
   </div>
-  <div className='w-[48%] border h-[60vh] bg-white px-4 py-6'>
+  <div className='w-[48%] border h-[64vh] bg-white px-4 py-6'>
   <p className='text-base leading-7 text-sky-700 font-sans font-normal drop-shadow-md '>ECIP welcomes the collaboration of individuals, institutions, and corporations who share our vision and are willing to support our mission.
      We are grateful for the support of the local community and appreciate their efforts in helping us achieve our goals. We urge everyone to join 
      us in our mission to provide care, support, and assistance 
@@ -19,11 +19,13 @@ function Engagement() {
     How to find us:
   </h1>
 
-  <div className=' flex flex-row flex-wrap py-6 justify-evenly'>
+  <div className=' flex flex-row flex-wrap py-6 justify-between'>
     <p className='w-[50%] mb-6 font-semibold'>Location <span className='font-normal' >&mdash; Tharaka-Nithi</span></p>
     <p className='w-[50%] mb-6 font-semibold'>Email <span className='font-normal'>&mdash; <a href='mailto:mumbuaesther033@gmail.com'>mumbuaesther033@gmail.com</a></span></p>
     <p className='w-[50%] mb-6 font-semibold'>Postal Office <span className='font-normal'>&mdash; P.O Box 266 60401 Chogoria</span></p>
     <p className='w-[50%] mb-6 font-semibold'>Tel <span className='font-normal'>&mdash;<a href='tel:0768938573'> +254 729 022 572</a> </span></p>
+    <p className='w-[50%] mb-6 font-semibold p-2 bg-slate-100 rounded-md'>Donate <span className='font-normal'>&mdash; EQUITY Bank <span className='flex flex-row font-semibold'>Paybill &mdash; <span className='font-normal'>247247</span></span>
+    <span className='flex flex-row font-semibold'>Account number <span className='font-normal'>&mdash; 1022**5633</span></span></span></p>
   </div>
 
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import {AiFillHeart} from 'react-icons/ai'
+import {SocialIcon}from 'react-social-icons'
+
 
 function Footer() {
   return (
-    <div className='h-[100vh] absolute bg-red-500  mt-[195rem] flex z-[999]' >
+    <div className='w-full h-[8vh] absolute mt-[225rem] ' >
+      <div  className='w-full h-full bg-sky-500 relative  flex flex-row items-center justify-center  '>
+      <p className='flex flex-row text-slate-100'>Developed with <span className='text-red-500 px-2 py-1 shadow-lg'><AiFillHeart/></span> for Humanity &copy; 2023</p>
+      
+      <div className="flex flex-row   justify-end px-4  w-max absolute right-2">
+           <SocialIcon style={{width:'2rem',height:'2rem'}} className='mx-1 bg-white overflow-hidden rounded-full transition ease-in-out hover:scale-110' url='https://twitter.com'/>
+                       <SocialIcon style={{width:'2rem',height:'2rem'}}  className='mx-1 bg-white overflow-hidden rounded-full transition ease-in-out hover:scale-110' url='https://instagram.com'/>
+                       <SocialIcon style={{width:'2rem',height:'2rem'}} className='mx-1 bg-white overflow-hidden rounded-full transition ease-in-out hover:scale-110' url='https://facebook.com'/>
+                       <SocialIcon style={{width:'2rem',height:'2rem'}} className='mx-1 bg-white overflow-hidden rounded-full transition ease-in-out hover:scale-110' url='https://linkedin.com'/>
+    </div>
+      </div>
+    
+
     
 
     </div>
